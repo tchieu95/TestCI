5a955b2e300c714e737e4637b2b59ad8217dfa99
Tran Chi Hieu <tchieu1995@gmail.com>
2022-08-20 16:08:54 +0700

    Initial Commit

---
+++ TestCI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCI
+//
+//  Created by HieuTC on 20/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
